66f4e04c1de0956c6c086b4ba1c18a469cb310dd
Fernando López <ferlopezm94@gmail.com>
2020-06-26 13:59:14 -0500

    feat(app): add basic cli commands using yargs

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,36 @@
-export const greetingUppercase = (name: string): string => {
-  const uppercase = `Hello ${name}!`.toUpperCase();
-  console.log(uppercase);
-  return uppercase;
-};
+import yargs from 'yargs';
+
+yargs.command(
+  'add',
+  'Add a new note',
+  {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string',
+    },
+    body: {
+      describe: 'Note body',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  argv => {
+    console.log('Title :>> ', argv.title);
+    console.log('Body :>> ', argv.body);
+  },
+);
+
+yargs.command('remove', 'Remove a note', () => {
+  console.log('Removing the note');
+});
+
+yargs.command('list', 'List your notes', () => {
+  console.log('Listing the notes');
+});
+
+yargs.command('read', 'Read a note', () => {
+  console.log('Reading the note');
+});
+
+yargs.parse();
